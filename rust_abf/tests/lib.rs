591b790a9fe6d6d166abcbab7d70141fb541f7d0
Luca Rossi <lucarossi147@gmail.com>
2023-07-20 00:05:43 +0200

    changed project structure

---
+++ rust_abf/tests/lib.rs
@@ -0,0 +1,49 @@
+#[cfg(test)]
+mod tests {
+    use rust_abf::*;
+
+    #[test]
+    fn test_get_file_signature_abf_v1() {
+        let result = get_file_signature("tests/test_abf/05210017_vc_abf1.abf");
+        match result {
+            Ok(r) =>  assert!(matches!(r, AbfType::AbfV1)),
+            _ => assert!(false),
+        }
+    }
+
+    #[test]
+    fn test_get_file_signature_abf_v2() {
+        let result = get_file_signature("tests/test_abf/18425108.abf");
+        match result {
+            Ok(r) =>  assert!(matches!(r, AbfType::AbfV2)),
+            _ => assert!(false),
+        }
+    }
+
+    #[test]
+    fn test_get_number_of_sweep_from_example_abf(){
+        let result = get_sweep_number("tests/test_abf/14o08011_ic_pair.abf");
+        match result {
+            Ok(r) =>  assert_eq!(3, r),
+            _ => assert!(false),
+        }
+    }
+
+    #[test]
+    fn test_get_number_of_sweep_from_abf_v1(){
+        let result = get_sweep_number("tests/test_abf/05210017_vc_abf1.abf");
+        match result {
+            Ok(r) =>  assert_eq!(10, r),
+            _ => assert!(false),
+        }
+    }
+
+    #[test]
+    fn test_get_number_of_sweep_from_abf_v2(){
+        let result = get_sweep_number("tests/test_abf/18425108.abf");
+        match result {
+            Ok(r) => assert_eq!(1, r),
+            _ => assert!(false),
+        }
+    }
+}
