0deb140a5230a5d63a0d19545aa1848fef29d9f6
Luca Rossi <lucarossi147@gmail.com>
2023-07-19 23:37:42 +0200

    added function to read sweep number and tests

--- rust_abf/src/lib.rs
+++ rust_abf/src/lib.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::Read;
+use std::io::{Read, Seek, SeekFrom};
 use std::str;
 
 pub enum AbfType {
@@ -24,8 +24,19 @@ pub fn get_file_signature(path: &str) -> Result<AbfType, std::io::Error> {
             _ => AbfType::Invalid,
             }
         _ => AbfType::Invalid,
-    })
-        
+    })       
+}
+
+pub fn get_sweep_number(path: &str)->Result<i32, std::io::Error>{
+    let mut file = File::open(path)?;
+    // Seek to the desired position (12 bytes offset)
+    file.seek(SeekFrom::Start(12))?;
+
+    // Read 4 bytes from the file into a buffer
+    let mut buffer = [0u8; 4];
+    file.read_exact(&mut buffer)?;
+    // Convert the bytes to an integer
+    Ok(i32::from_le_bytes(buffer))
 }
 
 #[cfg(test)]
@@ -33,7 +44,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_abf_v1() {
+    fn test_get_file_signature_abf_v1() {
         let result = get_file_signature("test_abf/05210017_vc_abf1.abf");
         match result {
             Ok(r) =>  assert!(matches!(r, AbfType::AbfV1)),
@@ -42,10 +53,37 @@ mod tests {
     }
 
     #[test]
-    fn test_abf_v2() {
+    fn test_get_file_signature_abf_v2() {
         let result = get_file_signature("test_abf/18425108.abf");
         match result {
             Ok(r) =>  assert!(matches!(r, AbfType::AbfV2)),
+            _ => println!("not found"),
+        }
+    }
+
+    #[test]
+    fn test_get_number_of_sweep_from_example_abf(){
+        let result = get_sweep_number("test_abf/14o08011_ic_pair.abf");
+        match result {
+            Ok(r) =>  assert_eq!(3, r),
+            _ => println!("not found"),
+        }
+    }
+
+    #[test]
+    fn test_get_number_of_sweep_from_abf_v1(){
+        let result = get_sweep_number("test_abf/05210017_vc_abf1.abf");
+        match result {
+            Ok(r) =>  assert_eq!(10, r),
+            _ => println!("not found"),
+        }
+    }
+
+    #[test]
+    fn test_get_number_of_sweep_from_abf_v2(){
+        let result = get_sweep_number("test_abf/18425108.abf");
+        match result {
+            Ok(r) => assert_eq!(1, r),
             _ => println!("not found"),
         }
     }
