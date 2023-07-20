bbbc82015897189d50add6c9dec4ccac1b74bfe4
Luca Rossi <lucarossi147@gmail.com>
2023-07-21 00:12:50 +0200

    testing out some file reading capabilities

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::{Read, Seek, SeekFrom};
 use std::str;
+use memmap::Mmap;
 
 pub enum AbfType {
     AbfV1,
@@ -8,16 +9,35 @@ pub enum AbfType {
     Invalid,
 }
 
+fn read_file_to_end(path: &str) -> Result<Vec<u8>, std::io::Error> {
+    let mut file = File::open(path)?;
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer)?;
+    // Process the buffer here
+    Ok(buffer)
+}
+
+fn mmap(path: &str) -> Result<Mmap, std::io::Error> {
+    Ok( unsafe {
+         Mmap::map(&File::open(path)?)? 
+        }
+    )
+}
+
+fn from_bytes_array_to_string(mmap: &Mmap, from: usize, len: usize) -> Result<&str, str::Utf8Error>{
+    str::from_utf8(&mmap[from..from+len])
+}
+
+fn from_bytes_array_to_string2(vec: &Vec<u8>, from: usize, len: usize) -> Result<&str, str::Utf8Error>{
+    str::from_utf8(&vec[from..from+len])
+}
+
 pub fn get_file_signature(path: &str) -> Result<AbfType, std::io::Error> {
-    let mut buffer = [0; 4];
-    // Open the file and read the first 4 bytes into a buffer
-    File::open(path)?.read_exact(&mut buffer)?;
-    // Convert the bytes to an ASCII string
-    let ascii_string = str::from_utf8(&buffer);
-    if let Ok(s) = ascii_string {
-        println!("{}", s);
-    }
-    Ok(match  ascii_string {
+    let mmap = mmap(path)?;
+    let file_signature = from_bytes_array_to_string(&mmap, 0, 4);
+    // let vec = read_file_to_end(path)?;
+    // let file_signature = from_bytes_array_to_string2(&vec, 0, 4);
+    Ok(match  file_signature {
         Ok(v) => match v {
             "ABF " => AbfType::AbfV1,
             "ABF2" => AbfType::AbfV2,
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,49 +1,60 @@
 #[cfg(test)]
 mod tests {
     use rust_abf::*;
+    use std::time::Instant;
 
     #[test]
     fn test_get_file_signature_abf_v1() {
+        let start_time = Instant::now();
         let result = get_file_signature("tests/test_abf/05210017_vc_abf1.abf");
-        match result {
-            Ok(r) =>  assert!(matches!(r, AbfType::AbfV1)),
-            _ => assert!(false),
-        }
+            match result {
+                Ok(r) =>  assert!(matches!(r, AbfType::AbfV1)),
+                _ => assert!(false),
+            }
+        println!("{:?}", start_time.elapsed());
     }
 
     #[test]
     fn test_get_file_signature_abf_v2() {
+        let start_time = Instant::now();
         let result = get_file_signature("tests/test_abf/18425108.abf");
         match result {
             Ok(r) =>  assert!(matches!(r, AbfType::AbfV2)),
             _ => assert!(false),
         }
+        println!("{:?}", start_time.elapsed());
     }
 
     #[test]
     fn test_get_number_of_sweep_from_example_abf(){
+        let start_time = Instant::now();
         let result = get_sweep_number("tests/test_abf/14o08011_ic_pair.abf");
         match result {
             Ok(r) =>  assert_eq!(3, r),
             _ => assert!(false),
         }
+        println!("{:?}", start_time.elapsed());
     }
 
     #[test]
     fn test_get_number_of_sweep_from_abf_v1(){
+        let start_time = Instant::now();
         let result = get_sweep_number("tests/test_abf/05210017_vc_abf1.abf");
         match result {
             Ok(r) =>  assert_eq!(10, r),
             _ => assert!(false),
         }
+        println!("{:?}", start_time.elapsed());
     }
 
     #[test]
     fn test_get_number_of_sweep_from_abf_v2(){
+        let start_time = Instant::now();
         let result = get_sweep_number("tests/test_abf/18425108.abf");
         match result {
             Ok(r) => assert_eq!(1, r),
             _ => assert!(false),
         }
+        println!("{:?}", start_time.elapsed());
     }
 }
