809797b3def35d709dc248dc2b2b27baefcfb754
Luca Rossi <lucarossi147@gmail.com>
2023-07-27 15:39:45 +0200

    added data read

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,44 @@ use std::str;
 use memmap::Mmap;
 use byteorder::{LittleEndian, ReadBytesExt};
 
+
+struct SectionProducer {
+    mmap: Mmap,
+}
+
+impl SectionProducer {
+    pub fn produce_from(&self, from: usize) -> Section {
+        Section::new(&self.mmap, from)
+    } 
+}
+
+struct Section<'a>{
+    mmap: &'a Mmap,
+    pub block_number: u32,
+    pub byte_count: u32,
+    pub item_count: u32, //todo this may be an i32 
+}
+
+impl<'a> Section<'a> {
+    pub fn new(mmap: &'a Mmap, from: usize) -> Self {
+        let block_number = 512 * from_byte_array_to_u32(mmap, from).unwrap();
+        let byte_count = from_byte_array_to_u32(mmap, from + std::mem::size_of::<u32>()).unwrap();
+        let item_count = from_byte_array_to_u32(&mmap, from + 2*(std::mem::size_of::<u32>())).unwrap();
+        Section { 
+            mmap,
+            block_number,
+            byte_count,
+            item_count,
+        }
+    }
+
+    pub fn read(&self) -> Vec<i16> {
+        let from = usize::try_from(self.block_number).unwrap();
+        let to = usize::try_from(self.block_number+self.item_count).unwrap();
+        self.mmap[from..=to].chunks_exact(2).map(|c|byte_array_to_i16(c)).collect()
+    } 
+}
+
 // TODO this will become an Abf Header
 // TODO the Abf Header will be an enum, of either abf_v1 or abf_v2 
 pub struct Abf{
@@ -20,6 +58,8 @@ pub struct Abf{
     pub crc_enable: u16,                    //  34
     pub file_crc: u32,                      //  38
     pub file_guid: u32,                     //  42
+
+    pub data: Vec<i16>,
 }
 
 impl Abf {
@@ -41,6 +81,33 @@ impl Abf {
         let crc_enable: u16 = from_byte_array_to_u16(&memmap, 34);
         let file_crc: u32 = from_byte_array_to_u32(&memmap, 38).unwrap();
         let file_guid: u32= from_byte_array_to_u32(&memmap, 42).unwrap();
+
+        // useful sections
+        let sec_prod = SectionProducer{mmap: memmap};
+        let protocol_section = sec_prod.produce_from(76);
+        let adc_section = sec_prod.produce_from(92);
+        let dac_section = sec_prod.produce_from(108);
+        let epoch_section = sec_prod.produce_from(124);
+        let adc_per_dac_section = sec_prod.produce_from(140);
+        let epoch_per_dac_section = sec_prod.produce_from(156);
+        let strings_section = sec_prod.produce_from(220);
+        let data_section = sec_prod.produce_from(236);
+        let tag_section = sec_prod.produce_from(252);
+
+        // TODO, create 2 possible abfs, one faster with only useful sections and one with all the possible sections
+        // not useful sections
+        let user_list_section = sec_prod.produce_from(172);
+        let stats_region_section = sec_prod.produce_from(188);
+        let math_section = sec_prod.produce_from(204);
+        let scope_section = sec_prod.produce_from(268);
+        let delta_section = sec_prod.produce_from(284);
+        let voice_tag_section = sec_prod.produce_from(300);
+        let synch_array_section = sec_prod.produce_from(316);
+        let annotation_section = sec_prod.produce_from(332);
+        let stats_section = sec_prod.produce_from(348);
+
+        // println!("{:?}", data_section.read().into_iter().take(10).collect::<Vec<i16>>());
+        let data = data_section.read();
         Abf {
             file_signature,
             file_version_number,
@@ -55,6 +122,7 @@ impl Abf {
             crc_enable,
             file_crc,
             file_guid,
+            data,
         }
     }
 }
@@ -87,9 +155,25 @@ fn from_byte_array_to_u32(mmap: &Mmap, from: usize) -> Result<u32, ()> {
     }
 }
 
+fn from_byte_array_to_i32(mmap: &Mmap, from: usize) -> Result<i32, ()> {
+    // Extract the relevant bytes (4 bytes) from the mmap slice
+    let mut bytes_slice = &mmap[from..from + std::mem::size_of::<i32>()];
+    // Convert the bytes to a u32 value in little-endian order
+    if let Ok(u32) = bytes_slice.read_i32::<LittleEndian>(){
+        Ok(u32)
+    } else {
+        Err(())
+    }
+}
+
 fn from_byte_array_to_u16(mmap: &Mmap, from: usize) -> u16 {
     let mut bytes_slice = &mmap[from..from + std::mem::size_of::<u16>()];
     bytes_slice.read_u16::<LittleEndian>().unwrap()
+}
+
+fn byte_array_to_i16(ba: &[u8]) -> i16 {
+    let mut ba = ba;
+    ba.read_i16::<LittleEndian>().unwrap()
 }
 
 fn get_file_signature(file_signature_str: &str) -> AbfType {
--- tests/lib.rs
+++ tests/lib.rs
@@ -3,72 +3,20 @@ mod tests {
     use rust_abf::*;
     use std::time::Instant;
 
-    // #[test]
-    // fn test_get_file_signature_abf_v1() {
-    //     let start_time = Instant::now();
-    //     let result = get_file_signature("tests/test_abf/05210017_vc_abf1.abf");
-    //         match result {
-    //             Ok(r) =>  assert!(matches!(r, AbfType::AbfV1)),
-    //             _ => assert!(false),
-    //         }
-    //     println!("{:?}", start_time.elapsed());
-    // }
-
-    // #[test]
-    // fn test_get_file_signature_abf_v2() {
-    //     let start_time = Instant::now();
-    //     let result = get_file_signature("tests/test_abf/18425108.abf");
-    //     match result {
-    //         Ok(r) =>  assert!(matches!(r, AbfType::AbfV2)),
-    //         _ => assert!(false),
-    //     }
-    //     println!("{:?}", start_time.elapsed());
-    // }
-
-    // #[test]
-    // fn test_get_number_of_sweep_from_example_abf(){
-    //     let start_time = Instant::now();
-    //     let result = get_sweep_number("tests/test_abf/14o08011_ic_pair.abf");
-    //     match result {
-    //         Ok(r) =>  assert_eq!(3, r),
-    //         _ => assert!(false),
-    //     }
-    //     println!("{:?}", start_time.elapsed());
-    // }
-
-    // #[test]
-    // fn test_get_number_of_sweep_from_abf_v1(){
-    //     let start_time = Instant::now();
-    //     let result = get_sweep_number("tests/test_abf/05210017_vc_abf1.abf");
-    //     match result {
-    //         Ok(r) =>  assert_eq!(10, r),
-    //         _ => assert!(false),
-    //     }
-    //     println!("{:?}", start_time.elapsed());
-    // }
-
-    // #[test]
-    // fn test_get_number_of_sweep_from_abf_v2(){
-    //     let start_time = Instant::now();
-    //     let result = get_sweep_number("tests/test_abf/18425108.abf");
-    //     match result {
-    //         Ok(r) => assert_eq!(1, r),
-    //         _ => assert!(false),
-    //     }
-    //     println!("{:?}", start_time.elapsed());
-    // }
-
     #[test]
     fn test_abfv2_1(){
         let start_time = Instant::now();
         let abf = Abf::new("tests/test_abf/14o08011_ic_pair.abf");
-        println!("{:?}", start_time.elapsed());
+        let elapsed_time = start_time.elapsed();
+        println!("{:?}", elapsed_time);
         assert!(matches!(abf.file_signature, AbfType::AbfV2));
         assert_eq!(abf.actual_episodes, 3);
         assert_eq!(abf.file_info_size, 512);
         assert_eq!(abf.file_start_date, 20141008);
         assert_eq!(abf.file_start_time_ms, 60198203);
         assert_eq!(abf.file_type, 1);
+        assert_eq!(abf.data[0], -2147);
+        // assert!(elapsed_time.as_millis()<2);
     }
 
     #[test]
@@ -84,16 +32,16 @@ mod tests {
         assert_eq!(abf.file_type, 1);
     }
 
-    #[test]
-    fn test_abfv1(){
-        let start_time = Instant::now();
-        let abf = Abf::new("tests/test_abf/05210017_vc_abf1.abf");
-        println!("{:?}", start_time.elapsed());
-        assert!(matches!(abf.file_signature, AbfType::AbfV1));
-        assert_eq!(abf.actual_episodes, 10);
-        assert_eq!(abf.file_info_size, 4236247045);
-        assert_eq!(abf.file_start_date, 6);
-        assert_eq!(abf.file_start_time_ms, 20050210);
-        assert_eq!(abf.file_type, 11985);
-    }
+    // #[test]
+    // fn test_abfv1(){
+    //     let start_time = Instant::now();
+    //     let abf = Abf::new("tests/test_abf/05210017_vc_abf1.abf");
+    //     println!("{:?}", start_time.elapsed());
+    //     assert!(matches!(abf.file_signature, AbfType::AbfV1));
+    //     assert_eq!(abf.actual_episodes, 10);
+    //     assert_eq!(abf.file_info_size, 4236247045);
+    //     assert_eq!(abf.file_start_date, 6);
+    //     assert_eq!(abf.file_start_time_ms, 20050210);
+    //     assert_eq!(abf.file_type, 11985);
+    // }
 }
