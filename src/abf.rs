d3d50bcfa89bc548afced104be54350ce509a28a
Luca Rossi <lucarossi147@gmail.com>
2023-08-22 11:40:05 +0200

    modified structs

--- src/abf.rs
+++ src/abf.rs
@@ -2,23 +2,30 @@ mod abf_v1;
 pub mod abf_v2;
 // use std::collections::HashMap;
 
+use std::collections::HashMap;
+
 use super::AbfKind;
 
 // TODO the abf will be a collection of sweeps.
 // sweeps will be common to both abfs
 // there won't be the necessity for an abf trait
 // the following struct are still a work in progress
-// struct Sweep{
-//     values: Vec<i32>,
-//     scaling_factor: f32,
+// enum FileType{
+//      I16,
+//      F32,  
+// }
+// struct Channel{
+//     values: Vec<i16>,
 //     uom: String,
+//     scaling_factor: f32,
 //     label: String,
 // }
-
 // struct Abf{
 //     abf_kind: AbfKind,
-//     channels: Vec<usize>,
-//     sweeps: HashMap<usize, Sweep>
+//     number_of_channels: u32,
+//     number_of_sweeps: u32,
+//     file_type: FileType,
+//     channels: HashMap<u32, Channel>,
 // }
 
 pub trait Abf {
