e4c3733ee2278b38d797e77e2c7cb618eeb0881f
Luca Rossi <lucarossi147@gmail.com>
2024-03-06 17:04:41 +0100

    major refactor and added get_path

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,47 @@
-use std::path::{Path, PathBuf};
-use std::fs::File;
-use std::io::{Error, ErrorKind};
-use memmap2::Mmap;
+// ! # Abf Crate
+// !
+// ! This crate defines the `Abf` struct, representing data from Axon Binary Format (ABF) files.
+// ! ABF files are typically used in electrophysiological recordings.
+// !
+// ! ## Example Usage
+// !
+// ! ```rust
+// ! use abf::{Abf, AbfKind};
+// ! use std::path::Path;
+// ! // Create an Abf instance
+// ! let abf = Abf::from_file(Path::new(filepath)).unwrap();
+// !
+// ! // Access information about the ABF file
+// ! println!("File Signature: {:?}", abf.get_file_signature());
+// ! println!("Channels Count: {}", abf.get_channels_count());
+// ! println!("Sweeps Count: {}", abf.get_sweeps_count());
+// !
+// ! // Access data from the ABF file
+// ! abf.get_channels()
+// ! .map(|c| c.get_sweeps()) 
+// ! .flatten()
+// ! .for_each(|s| assert_eq!(s.unwrap().len(), 250_000));
+// ! let channel_data = abf.get_sweep_in_channel(0, 0);
+// ! if let Some(data) = channel_data {
+// !     println!("Channel 0, Sweep 0 data: {:?}", data);
+// ! }
+// ! ```
 
-pub mod abf;
+use memmap2::Mmap;
+use std::{
+    collections::HashMap, 
+    path::{Path, PathBuf},
+    fs::File,
+    io::{Error, ErrorKind}
+};
+use channel::Channel;
+
 mod conversion_util;
 use conversion_util as cu;
-use abf::Abf;
+mod abf_v1;
+pub mod abf_v2;
+mod channel;
+
 // use abf::abf_v2::AbfV2;
 // TODO this will become an Abf Header
 // TODO the Abf Header will be an enum, of either abf_v1 or abf_v2 
@@ -16,9 +51,18 @@ pub enum AbfKind {
     AbfV2,
 }
 
-pub struct AbfBuilder;
+/// The `Abf` struct represents data from an ABF file.
+pub struct Abf {
+    abf_kind: AbfKind,
+    channels_count: u32,
+    sweeps_count: u32,
+    sampling_rate: f32,
+    channels: HashMap<u32, Channel>,
+    path: PathBuf,
+}
 
-impl AbfBuilder {
+impl Abf {
+
     pub fn from_file(filepath: &Path)-> Result<Abf, Error> {
         let path = PathBuf::from(filepath);
         let memmap =  unsafe { Mmap::map(&File::open(&path)?)? };
@@ -32,4 +76,52 @@ impl AbfBuilder {
             _ => Err(Error::new(ErrorKind::InvalidData, "Incorrect file type"))
         }
     }
+
+    pub fn get_time_axis(&self) -> Vec<f32> {
+        let data_sec_per_point = 1.0 / self.sampling_rate;
+        let data_len = self.get_sweep_in_channel(0, 0).unwrap().len();
+        let number_of_points = data_len / self.sweeps_count as usize;
+        (0..number_of_points).map(|n| n as f32).map(|n| n * data_sec_per_point).collect()
+    }
+
+    pub fn get_channels_count(&self) -> u32 {
+        self.channels_count
+    }
+
+    pub fn get_sweeps_count(&self ) -> u32 {
+        self.sweeps_count
+    }
+
+    pub fn get_sweep_in_channel(&self, sweep: u32, channel: u32) -> Option<Vec<f32>> {
+        if sweep >= self.sweeps_count {
+            return None;
+        }
+        self.channels.get(&channel)?.get_sweep(sweep)
+    }
+
+    pub fn get_file_signature(&self) -> AbfKind {
+        self.abf_kind
+    }
+
+    pub fn get_channel(&self, index: u32) -> Option<&Channel> {
+        self.channels.get(&index)
+    }
+
+    pub fn get_channels(&self) -> impl Iterator<Item = &Channel>{
+        self.channels.values().into_iter()
+    }
+    
+    pub fn get_sampling_rate(&self) -> f32 {
+        self.sampling_rate
+    }
+
+    pub fn get_path(&self) -> &Path {
+        &self.path
+    }
 }
+
+// pub trait Abf {
+//     fn get_channel_count(&self) -> usize;
+//     fn get_data(&self, channel: usize) -> Option<Vec<f32>>;
+//     fn get_file_signature(&self) -> AbfKind;
+// }
